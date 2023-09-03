dfec632d275d300a7e0fbb49521d601f9a3d5a73
kretktz <kretktz@gmail.com>
2023-09-03 18:44:04 +0900

    Added Menu SFX

---
+++ Assets/Scripts/btnFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnFX : MonoBehaviour
+{
+    public AudioSource menuFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        menuFx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        menuFx.PlayOneShot(clickFx);
+    }
+}
