dfc8792acb7fd919b08d348c9d34411bebb73b13
kretktz <kretktz@gmail.com>
2023-09-03 14:59:12 +0900

    Updates to Dialogue Box design

---
+++ Assets/DialogueBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    public Transform boxSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -15,12 +15,18 @@ public class DialogueManager : MonoBehaviour
     int activeMessage = 0;
     public static bool isActive = false;
 
+    private Vector3 invisible = new Vector3(0, 0, 0);
+    private Vector3 visible = new Vector3(1, 1, 1);
+
     public void OpenDialogue(Message[] messages, Actor[] actors)
     {
         currentMessages = messages;
         currentActors = actors;
         activeMessage = 0;
         isActive = true;
+
+        backgroundBox.localScale = visible;
+
         Debug.Log("Started conversation. Loaded messages: " + messages.Length);
         DisplayMessage();
     }
@@ -46,18 +52,20 @@ public class DialogueManager : MonoBehaviour
         {
             Debug.Log("Convo ended");
             isActive = false;
+            backgroundBox.localScale = invisible;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        backgroundBox.localScale = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.E) && isActive == true)
         {
             NextMessage();
